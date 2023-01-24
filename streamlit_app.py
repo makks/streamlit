565c9d526444582ef4dd169a0e4814342f00ad46
makks <makks@hotmail.com>
2023-01-24 20:05:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents new healthy dinner')
 streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, spinach & rocket Smoothie')
-streamlit.text('Hard-boiled free-range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, spinach & rocket Smoothie')
+streamlit.text('🐔 Hard-boiled free-range Egg')
+streamlit.text('🥑🍞 Advocado Toast')
