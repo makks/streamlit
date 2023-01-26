4de0a1e58a23306719f691eebdeeda0f803b51d7
makks <makks@hotmail.com>
2023-01-26 11:46:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 # Create a function
 # This one gets data from the fruityvice API and returns a normalized table
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized 
